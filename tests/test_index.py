98b5148cbb5f73bcab39cf239afedf44049c1214
Foo Barstein <amos@cs.nyu.edu>
2020-06-10 17:21:19 -0400

    convert link text to lowercase

--- tests/test_index.py
+++ tests/test_index.py
@@ -14,12 +14,12 @@ from selenium.webdriver.common.keys import Keys
 class Tests:
 
   @pytest.fixture(scope="class")
-  def site_settings(self):
+  def settings(self):
     settings = json.load(open('./settings.json', 'r'))
     yield settings
 
   @pytest.fixture(scope="class")
-  def web_driver(self):
+  def driver(self):
     """
     Pop open a web browser and make it available to the tests.
     """
@@ -34,43 +34,43 @@ class Tests:
     # now tear it down
     driver.close()
 
-  def test_title(self, web_driver, site_settings):
+  def test_title(self, driver, settings):
     """
     Check the title tag for the correct text
     """
-    assert site_settings["name"] in web_driver.title
+    assert settings["name"] in driver.title
 
-  def test_h1(self, web_driver, site_settings):
+  def test_h1(self, driver, settings):
     """
     Check the h1 tag for the correct text
     """
-    elem = web_driver.find_element_by_tag_name("h1")
-    assert site_settings["name"] in elem.text
+    elem = driver.find_element_by_tag_name("h1")
+    assert settings["name"] in elem.text
 
-  def test_ol_exists(self, web_driver, site_settings):
+  def test_ol_exists(self, driver, settings):
     """
     Check that the order list items exists
     """
-    elems = web_driver.find_elements_by_css_selector("ol li") # find the h1 tag
+    elems = driver.find_elements_by_css_selector("ol li") # find the h1 tag
     assert len(elems) >= 2
 
-  def test_link_href_exists(self, web_driver):
+  def test_link_href_exists(self, driver):
     """
     Check url of links to all assignment pages.
     """
     target_urls = ['index.html', 'about_me.html', 'topic_of_interest.html', 'user_experience_design.html', 'professional_site.html', 'animated_gif.html', 'video.html']
     for url in target_urls:
       # check for hrefs with either single or double quotes
-      elem_option1 = web_driver.find_element_by_xpath('//a[@href="' + url + '"]')
-      elem_option2 = web_driver.find_element_by_xpath("//a[@href='" + url + "']")
+      elem_option1 = driver.find_element_by_xpath('//a[@href="' + url + '"]')
+      elem_option2 = driver.find_element_by_xpath("//a[@href='" + url + "']")
       assert elem_option1 or elem_option2 # check that it exists
 
-  def test_link_text_exists(self, web_driver):
+  def test_link_text_exists(self, driver):
     """
     Check text of links to all assignment pages.
     """
     target_terms = ['UNIX', 'HTML', 'CSS', 'JQuery', 'User Experience', 'Responsive Design', 'Bootstrap', 'Animated GIF', 'Digital Video']
+    elems = [x.text.strip().lower().replace('assignment', '') for x in driver.find_elements_by_css_selector("a")]
+    elems = ''.join(elems)
     for term in target_terms:
-      elem = web_driver.find_element_by_partial_link_text(term)
-      assert elem # check that a matching element exists
-
+      assert term.lower() in elems
